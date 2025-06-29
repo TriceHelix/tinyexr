237e6c523b05c606e527ec95511bc285598ad381
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2025-06-28 22:43:41 -0600

    SH: Add extra guards to compile_to_js.sh.

--- experimental/js/compile_to_js.sh
+++ experimental/js/compile_to_js.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 EMCC=emcc
 EMCXX=em++
@@ -9,6 +10,6 @@ EMCXX=em++
 #${EMCC} -std=c++11 --bind -O2 -I../../ binding.cc --memory-init-file 0 -s TOTAL_MEMORY=67108864 -s DEMANGLE_SUPPORT=1 -s EXPORTED_FUNCTIONS="['_ParseEXRHeaderFromMemory', '_LoadEXRFromMemory']" -o tinyexr.js
 #${EMCC} --bind -Os -I../../ -I../../deps/miniz binding.cc ../../deps/miniz/miniz.c --memory-init-file 0 -s TOTAL_MEMORY=67108864 -o tinyexr.js
 
-${EMCC} -Os -I../../deps/miniz ../../deps/miniz/miniz.c -c -o miniz.o
-${EMCXX} --bind -Os -I../../ -I../../deps/miniz binding.cc miniz.o -s ALLOW_MEMORY_GROWTH=1 -o tinyexr.js --emit-tsd tinyexr.d.ts
+"${EMCC}" -Os "-I../../deps/miniz" "../../deps/miniz/miniz.c" -c -o miniz.o
+"${EMCXX}" --bind -Os "-I../../" "-I../../deps/miniz" binding.cc miniz.o -s ALLOW_MEMORY_GROWTH=1 -o tinyexr.js --emit-tsd tinyexr.d.ts
 
